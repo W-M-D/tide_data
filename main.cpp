cd59842792b50f3ebe990dc2e11254f60b5cf9ed
w-m-d <matthew.d.weger@gmail.com>
2015-04-02 14:51:47 -0400

    adding make file and main

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "CTIDE_STATION.h"
+using namespace std;
+string weather_station = "8727520";
+CTIDE_STATION tide_station(0,weather_station);
+int main()
+{
+    tide_station.fetch_predictive_tide_data_day();
+    tide_station.fetch_recent_predictive_tide_data();
+    tide_station.print_event_data();
+    return 0;
+}
